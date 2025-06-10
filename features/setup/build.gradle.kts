19698c7248cd7a3f345fce7dc3c2f562bf8ef92b
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-10 11:20:37 +0800

    setup: add initial setup feature module

--- features/setup/build.gradle.kts
+++ features/setup/build.gradle.kts
@@ -1,22 +1,19 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
 }
 
 android {
-    namespace = "com.ralphmarondev.maron_os"
+    namespace = "com.ralphmarondev.setup"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.ralphmarondev.maron_os"
         minSdk = 28
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -42,13 +39,9 @@ android {
 
 dependencies {
 
-    // core
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
     implementation(project(":core:presentation"))
-
-    // features
-    implementation(project(":features:setup"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -60,7 +53,6 @@ dependencies {
     implementation(libs.androidx.material3)
 
     implementation(libs.androidx.material.icons.extended.android)
-    implementation(libs.androidx.core.splashscreen)
     implementation(libs.coil.compose)
     implementation(libs.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
@@ -69,8 +61,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
