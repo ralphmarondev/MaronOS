cf0a912705ed9e3498e42af77c577fef2dea9745
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-10 19:10:10 +0800

    auth: implement LoginViewModel

--- features/auth/src/main/java/com/ralphmarondev/auth/presentation/login/LoginViewModel.kt
+++ features/auth/src/main/java/com/ralphmarondev/auth/presentation/login/LoginViewModel.kt
@@ -1,7 +1,31 @@
 package com.ralphmarondev.auth.presentation.login
 
 import androidx.lifecycle.ViewModel
+import com.ralphmarondev.domain.model.Result
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class LoginViewModel : ViewModel() {
 
+    private val _username = MutableStateFlow("")
+    val username = _username.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password = _password.asStateFlow()
+
+    private val _response = MutableStateFlow<Result?>(null)
+    val response = _response.asStateFlow()
+
+
+    fun onUsernameValueChange(value: String) {
+        _username.value = value
+    }
+
+    fun onPasswordValueChange(value: String) {
+        _password.value = value
+    }
+
+    fun login() {
+
+    }
 }
