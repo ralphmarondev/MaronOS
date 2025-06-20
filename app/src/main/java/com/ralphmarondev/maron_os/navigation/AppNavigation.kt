df25377fae3e6fb02c0389cee9b80aec897f372a
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-20 17:17:11 +0800

    app: update navigation, hide custom splash

--- app/src/main/java/com/ralphmarondev/maron_os/navigation/AppNavigation.kt
+++ app/src/main/java/com/ralphmarondev/maron_os/navigation/AppNavigation.kt
@@ -1,24 +1,35 @@
 package com.ralphmarondev.maron_os.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ralphmarondev.auth.AuthNavigation
 import com.ralphmarondev.browser.BrowserNavigation
+import com.ralphmarondev.data.local.preferences.AppPreferences
 import com.ralphmarondev.maron_os.launcher.presentation.LauncherScreen
 import com.ralphmarondev.maron_os.splash.presentation.SplashScreen
 import com.ralphmarondev.settings.SettingsNavigation
 import com.ralphmarondev.setup.SetupNavigation
+import org.koin.compose.koinInject
 
 @Composable
 fun AppNavigation(
     navController: NavHostController = rememberNavController()
 ) {
+    val preference = koinInject<AppPreferences>()
+    val isFirstLaunch = preference.isFirstLaunch.collectAsState(initial = true).value
+    val startDestination = if (isFirstLaunch) {
+        Routes.Auth
+    } else {
+        Routes.Launcher
+    }
+
     NavHost(
         navController = navController,
-        startDestination = Routes.Splash
+        startDestination = startDestination
     ) {
         composable<Routes.Splash> {
             SplashScreen(
