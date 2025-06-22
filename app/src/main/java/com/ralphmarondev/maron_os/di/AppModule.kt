af9e884d5215646d4b2aec6e25bff626f421451a
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-22 08:39:07 +0800

    app: register noteModule to appModule

--- app/src/main/java/com/ralphmarondev/maron_os/di/AppModule.kt
+++ app/src/main/java/com/ralphmarondev/maron_os/di/AppModule.kt
@@ -3,6 +3,7 @@ package com.ralphmarondev.maron_os.di
 import com.ralphmarondev.auth.di.authModule
 import com.ralphmarondev.data.di.dataModule
 import com.ralphmarondev.maron_os.launcher.di.launcherModule
+import com.ralphmarondev.notes.di.noteModule
 import com.ralphmarondev.setup.di.setupModule
 
 val appModule = listOf(
@@ -10,4 +11,5 @@ val appModule = listOf(
     setupModule,
     authModule,
     launcherModule,
+    noteModule
 )
