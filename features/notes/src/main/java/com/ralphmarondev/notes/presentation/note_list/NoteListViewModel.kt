b315d36a3910c025145fac7f2b3ae30fef4d8f17
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-22 09:35:27 +0800

    notes: show notes from database on NoteListScreen

--- features/notes/src/main/java/com/ralphmarondev/notes/presentation/note_list/NoteListViewModel.kt
+++ features/notes/src/main/java/com/ralphmarondev/notes/presentation/note_list/NoteListViewModel.kt
@@ -1,31 +1,26 @@
 package com.ralphmarondev.notes.presentation.note_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ralphmarondev.notes.domain.model.Note
+import com.ralphmarondev.notes.domain.usecase.GetAllNotesUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class NoteListViewModel : ViewModel() {
+class NoteListViewModel(
+    private val getAllNotesUseCase: GetAllNotesUseCase
+) : ViewModel() {
 
     private val _notes = MutableStateFlow<List<Note>>(emptyList())
     val notes = _notes.asStateFlow()
 
 
     init {
-        _notes.value += Note(
-            id = 1,
-            title = "Hello",
-            caption = "World"
-        )
-        _notes.value += Note(
-            id = 2,
-            title = "Hi",
-            caption = "Kate"
-        )
-        _notes.value += Note(
-            id = 3,
-            title = "Hello",
-            caption = "Ralph"
-        )
+        viewModelScope.launch {
+            getAllNotesUseCase().collect { noteList ->
+                _notes.value = noteList
+            }
+        }
     }
 }
