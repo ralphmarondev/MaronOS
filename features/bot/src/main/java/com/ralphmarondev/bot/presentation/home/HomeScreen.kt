3ba39027b2c6ebf2e0f4ec7220bfbb625915caa8
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-24 17:47:06 +0800

    bot: add input field

--- features/bot/src/main/java/com/ralphmarondev/bot/presentation/home/HomeScreen.kt
+++ features/bot/src/main/java/com/ralphmarondev/bot/presentation/home/HomeScreen.kt
@@ -1,22 +1,36 @@
 package com.ralphmarondev.bot.presentation.home
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+    val viewModel: HomeViewModel = koinViewModel()
+    val message = viewModel.message.collectAsState().value
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -30,6 +44,44 @@ fun HomeScreen() {
                     titleContentColor = MaterialTheme.colorScheme.onPrimary
                 )
             )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    OutlinedTextField(
+                        value = message,
+                        onValueChange = viewModel::onMessageValueChange,
+                        placeholder = {
+                            Text(
+                                text = "Enter message",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                        },
+                        trailingIcon = {
+                            IconButton(onClick = viewModel::send) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Outlined.Send,
+                                    contentDescription = "Send",
+                                    tint = MaterialTheme.colorScheme.secondary
+                                )
+                            }
+                        },
+                        textStyle = TextStyle(
+                            fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                            fontWeight = MaterialTheme.typography.titleMedium.fontWeight,
+                            color = MaterialTheme.colorScheme.secondary
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    )
+
+                }
+            }
         }
     ) { innerPadding ->
         LazyColumn(
--- features/bot/src/main/java/com/ralphmarondev/bot/presentation/home/HomeViewModel.kt
+++ features/bot/src/main/java/com/ralphmarondev/bot/presentation/home/HomeViewModel.kt
@@ -1,6 +1,20 @@
 package com.ralphmarondev.bot.presentation.home
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class HomeViewModel : ViewModel() {
+
+    private val _message = MutableStateFlow("")
+    val message = _message.asStateFlow()
+
+
+    fun onMessageValueChange(value: String) {
+        _message.value = value
+    }
+
+    fun send() {
+
+    }
 }
