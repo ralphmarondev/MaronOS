068aed3658ba18354e4790a43b1a009e28f81055
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-24 18:41:36 +0800

    bot: configure bot to remember previous chats

--- features/bot/src/main/java/com/ralphmarondev/bot/presentation/home/HomeViewModel.kt
+++ features/bot/src/main/java/com/ralphmarondev/bot/presentation/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
 import com.ralphmarondev.bot.domain.model.Message
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -13,7 +14,7 @@ class HomeViewModel : ViewModel() {
 
     val generativeModel = GenerativeModel(
         modelName = "gemini-2.5-flash",
-        apiKey = "api_key_goes_here_hehe"
+        apiKey = ""
     )
 
     private val _message = MutableStateFlow("")
@@ -29,19 +30,32 @@ class HomeViewModel : ViewModel() {
 
     fun send() {
         viewModelScope.launch {
-            val chat = generativeModel.startChat()
+            try {
+                val chat = generativeModel.startChat(
+                    history = _conversation.value.map {
+                        content(if (it.role == "kate") "model" else it.role) { text(it.message) }
+                    }.toList()
+                )
 
-            _conversation.value += Message(
-                message = _message.value.trim(),
-                role = "user"
-            )
+                _conversation.value += Message(
+                    message = _message.value.trim(),
+                    role = "user"
+                )
 
-            val response = chat.sendMessage(_message.value)
-            _conversation.value += Message(
-                message = response.text.toString(),
-                role = "kate"
-            )
-            Log.d("App", "Response: ${response.text.toString()}")
+                val response = chat.sendMessage(_message.value)
+                _conversation.value += Message(
+                    message = response.text.toString(),
+                    role = "kate"
+                )
+                Log.d("App", "Response: ${response.text.toString()}")
+            } catch (e: Exception) {
+                _conversation.value += Message(
+                    message = "Sorry, I don't understand you.",
+                    role = "kate"
+                )
+                Log.e("App", "Error: ${e.message}")
+            }
+            _message.value = ""
         }
     }
 }
