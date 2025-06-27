0afb1b7204a24f970ffb3880b9215907b679113a
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-27 12:32:30 +0800

    core: fix missing annotation on BotMessageDao

--- core/data/src/main/java/com/ralphmarondev/data/local/database/dao/BotMessageDao.kt
+++ core/data/src/main/java/com/ralphmarondev/data/local/database/dao/BotMessageDao.kt
@@ -1,6 +1,8 @@
 package com.ralphmarondev.data.local.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ralphmarondev.data.local.database.entity.BotMessageEntity
 import kotlinx.coroutines.flow.Flow
@@ -8,6 +10,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface BotMessageDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun create(botMessageEntity: BotMessageEntity)
 
     @Query("SELECT * FROM bot_message")
