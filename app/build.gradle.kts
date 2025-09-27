4eb9841bd40720040be00649292b77f93ba7ba8a
Ralph Maron A. Eda <128874876+ralphmarondev@users.noreply.github.com>
2025-09-27 18:07:10 +0800

    core: add common module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
     implementation(project(":core:presentation"))
+    implementation(project(":core:common"))
 
     // features
     implementation(project(":features:setup"))
